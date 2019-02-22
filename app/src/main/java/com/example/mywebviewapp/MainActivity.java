04cff8b8a96c3fa8964d9ec8bec592d3bbb109e8
ebautista2 <edgar.bautista@coxautoinc.com>
2019-02-22 17:15:18 -0500

    init

---
+++ app/src/main/java/com/example/mywebviewapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.mywebviewapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = findViewById(R.id.activity_main_webview);
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient());
+
+        mWebView.loadUrl("https://sellerdashboard.manheim.man-ba.com/login/new");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
